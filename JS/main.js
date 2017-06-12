f506368572074f5fb693b8b39f242203d99b0965
Peter J. Bates <peterbates8@gmail.com>
2017-06-12 17:04:03 -0500

    Added resize funtionality and fixed overflow.

--- JS/main.js
+++ JS/main.js
@@ -2,9 +2,9 @@ var gridSize = 16;
 
 
 $(document).ready(function () {
-    
+       
+       
 function createGrid() {
-	
 	for (var i = 0; i < gridSize * gridSize; i++) {
 		$('#grid').append('<div class="square"></div>')
 	}
@@ -18,12 +18,28 @@ createGrid();
 
 
 function pen() {
-  	
   $('.square').mouseenter(function() {
     $(this).css('background-color', "#000");
    });
  }
 pen();
 
+
+function reset() {
+	$('#reset').click(function() {
+  	$('.square').css('background-color', "#fff");
+  });
+}
+reset();
+
+
+function resize() {
+	$('#size').click(function() {
+  	gridSize = (prompt('How fine would you like your brush? Enter a number from 1-100.'));
+   createGrid();
+   pen();
+  });
+}
+resize();
 
 });
