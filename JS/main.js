cf827071f6fe94a4758d5dfc36db73b7c26af7c1
Peter J. Bates <peterbates8@gmail.com>
2017-06-12 16:26:38 -0500

    Added basic style to css. Added grid, pen, and reset functionality to js.

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,29 @@
 var gridSize = 16;
-var color = "#000";
 
 
 $(document).ready(function () {
-    createGrid();
-    pickColor();
-    brush();
-});
-
+    
 function createGrid() {
-    for (var i = 0, i < Math.pow(gridSize, 2), i++) {
-         $('#grid').append('<div class="square"></div>');
-    }
-   
+	
+	for (var i = 0; i < gridSize * gridSize; i++) {
+		$('#grid').append('<div class="square"></div>')
+	}
+
+	$('.square').css({
+		'height' : (500 / gridSize) + "px",
+		'width' : (500 / gridSize) +'px',
+	});
 }
+createGrid();  
+
+
+function pen() {
+  	
+  $('.square').mouseenter(function() {
+    $(this).css('background-color', "#000");
+   });
+ }
+pen();
+
+
+});
